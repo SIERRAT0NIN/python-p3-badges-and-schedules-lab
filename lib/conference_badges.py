21086ead9d87eea0a96c0b4412c63fbe607a83b0
Alberto Sierra <Sierra.Alberto@iCloud.com>
2023-10-28 14:00:08 -0700

    complete badges

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,32 @@
+NAMES = ["alberto", "austin"]
+
+
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
+
 
 def batch_badge_creator(names):
-    return None
+    badge_message = []
+    for name in names:
+        badge_message.append(f"Hello, my name is {name}.")
+    return badge_message
+
+
+batch_badge_creator(NAMES)
+
 
 def assign_rooms(names):
-    return None
+    rooms = [
+        f"Hello, {name}! You'll be assigned to room {room}!"
+        for room, name in enumerate(names, start=1)
+    ]
+    return rooms
+
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    rooms_assignment = assign_rooms(names)
+    for badge in badges:
+        print(badge)
+    for room in rooms_assignment:
+        print(room)
